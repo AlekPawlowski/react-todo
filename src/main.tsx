960fda7b633a8842a21ab56b13040a68c72a3168
AlekPawlowski <alekpawlowski21@gmail.com>
2023-10-20 17:08:11 +0200

    add error boundry

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux'
 
 import App from './App.tsx'
 import { store } from './redux/index.ts'
+import ErrorBoundary from './components/ErrorBoundry/ErrorBoundry.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ErrorBoundary>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ErrorBoundary>
+    </React.StrictMode>,
 )
